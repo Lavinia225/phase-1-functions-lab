a7bfbb35521f9aabd0f29b7bf8aaec751d1517d1
Luna Seifert <thepuddingdeity@gmail.com>
2022-09-22 17:23:02 -0600

    commit tree f7f4c3f065a29c20e4f1da5a26942f6bd0389473

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(input){ //264ft is one block. HQ is on 42nd street
+
+    if (input > 42){
+        return input - 42;
+    }
+    else if (input < 42){
+        return 42 - input;
+    }
+}
+
+function distanceFromHqInFeet(input){
+    const blockCount = distanceFromHqInBlocks(input);
+    return blockCount * 264;
+
+}
+
+function distanceTravelledInFeet(start, destination){
+    let totalDistance;
+    if (start > destination){
+        totalDistance = start - destination;
+    }
+    else if (destination > start){
+        totalDistance = destination - start;
+    }
+    return totalDistance * 264;
+}
+
+function calculatesFarePrice(start, destination){
+    let totalDistance = distanceTravelledInFeet(start, destination);
+
+    if (totalDistance < 400){
+        return 0;
+    }
+
+    else if (totalDistance > 2500){
+        return "cannot travel that far";
+    }
+
+    else if (totalDistance > 2000 && totalDistance < 2500){
+        return 25;
+    }
+
+    else {
+        return (totalDistance - 400) * .02;
+    }}
